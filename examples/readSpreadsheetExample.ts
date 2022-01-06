58421eacc8bbe1f977f40b1f8d604d2fc86c11a8
Suhwan Jee <jsh56son@gmail.com>
2022-01-06 14:50:36 +0900

    Refactor: examples 폴더 추가

--- examples/readSpreadsheetExample.ts
+++ examples/readSpreadsheetExample.ts
@@ -1,4 +1,4 @@
-import getSpreadsheetData from './spreadsheet';
+import getSpreadsheetData from '../spreadsheet';
 
 async function main() {
   const data = await getSpreadsheetData('config/credentials.json', '19v4zuIp_mQevrB4-9Bdbw68KlDq3niK5kNXTMbrW-jU', '설문지 응답 시트1!A2:ZZ');
